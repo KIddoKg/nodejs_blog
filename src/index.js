e80025b1c5dc47ee274e0322bbd397288162c012
KIddoKg <freedomv27@gmail.com>
2021-10-15 15:33:47 +0700

    master

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(morgan('combined'))
+
+// template engine
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'))
+
+// "/" mean route
+app.get('/helu', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
